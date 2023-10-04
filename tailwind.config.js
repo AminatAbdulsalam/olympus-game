251b713d88be34789a11c1da8519144374384966
Aminat Abdulsalam <aminatabdulsalamsmith@gmail.com>
2023-10-04 13:59:17 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        main: '#2D2E42',
+        light: '#9887CE',
+        inner: '#3E394C',
+        yellow: '#FFBD3D'
+      },
+    },
+  },
+  plugins: [],
+}
+
